894726f71f2b4ca68ed974be11a3718f728d08c2
saurabhgunturkar <saurabhgunturkar07@gmail.com>
2025-02-22 13:27:32 +0530

    webdriver updated

--- src/test/java/Utilities/Base.java
+++ src/test/java/Utilities/Base.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -16,7 +17,9 @@ public class Base {
     public static WebDriver localBrowser(String browsername) {
         switch (browsername.toLowerCase()) {
             case "chrome":
-                driver = new ChromeDriver();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--headless");
+                driver = new ChromeDriver(options);
                 break;
             case "edge":
                 driver=new EdgeDriver();
